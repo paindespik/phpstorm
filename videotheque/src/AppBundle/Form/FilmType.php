15129640a4a1236b729eb0a77b68addbe0de0d50
paindespik <seandriard3@gmail.com>
2019-11-15 12:13:07 +0100

    new commit

---
+++ videotheque/src/AppBundle/Form/FilmType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FilmType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('titre')->add('annee')->add('resume')->add('codepays')->add('idrealisateur')->add('genre')->add('idacteur');
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Film'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_film';
+    }
+
+
+}
